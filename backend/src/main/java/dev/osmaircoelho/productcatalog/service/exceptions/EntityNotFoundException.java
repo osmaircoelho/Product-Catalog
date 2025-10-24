3d89826435a031fab94a1235cc7b62d2fa2abf28
Osmair Coelho <osmair.coelho@gmail.com>
2025-10-24 11:09:02 -0300

    criado exceptionHandler para todos so controller

--- backend/src/main/java/dev/osmaircoelho/productcatalog/service/exceptions/EntityNotFoundException.java
+++ backend/src/main/java/dev/osmaircoelho/productcatalog/service/exceptions/EntityNotFoundException.java
@@ -1,8 +1,12 @@
 package dev.osmaircoelho.productcatalog.service.exceptions;
 
+
+import java.io.Serial;
+
 public class EntityNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID =1L;
     public EntityNotFoundException(String msg){
         super(msg);
     }
 }
-
