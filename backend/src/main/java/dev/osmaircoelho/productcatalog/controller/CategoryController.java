921d1d6a47c1478ea785c610c2178848b224d45f
Osmair Coelho <osmair.coelho@gmail.com>
2025-10-24 11:40:39 -0300

    insert new category

--- backend/src/main/java/dev/osmaircoelho/productcatalog/controller/CategoryController.java
+++ backend/src/main/java/dev/osmaircoelho/productcatalog/controller/CategoryController.java
@@ -5,11 +5,10 @@ import dev.osmaircoelho.productcatalog.model.Category;
 import dev.osmaircoelho.productcatalog.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,17 +19,27 @@ public class CategoryController {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll(){
-
+    public ResponseEntity<List<CategoryDTO>> findAll() {
         List<CategoryDTO> list = service.findAll();
-
         return ResponseEntity.ok().body(list);
-
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
         CategoryDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    //code HTTP 201 recurso criado
+    @PostMapping
+    public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(dto.getId())
+                .toUri();
+
         return ResponseEntity.ok().body(dto);
     }
 
